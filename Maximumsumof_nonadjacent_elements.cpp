90b592249b0158677d558b1df53075f5e50936bf
Piyush <piyush.12109255@lpu.in>
2024-01-25 00:16:55 +0530

    maxm sum of non adjacent element

--- Maximumsumof_nonadjacent_elements.cpp
+++ Maximumsumof_nonadjacent_elements.cpp
@@ -1,7 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
+
+int help(vector<int>&nums,int n,vector<int>&dp)
 {
+    if(n==0) return nums[0];
+    if(n<0) return 0;
     
+    if(dp[n]!=-1) return dp[n];
+    int include=nums[n]+help(nums,n-2,dp);
+    int exclude=help(nums,n-1,dp)+0;
+    dp[n]=max(include,exclude);
+    return dp[n];
+
+    //return max(include,exclude);
+}
+int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    int ans=0;
+    int n=nums.size();
+    vector<int>dp(n+1,-1);
+    return help(nums,n-1,dp);
+}
+int main()
+{    
+    vector<int>nums={1,2,3,4,5};
+     int ans=0;
+    int n=nums.size();
+    vector<int>dp(n+1,-1);
+     cout<<help(nums,n-1,dp);
 }
